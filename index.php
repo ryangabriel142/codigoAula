3e33cefd62f708cc13cc3ba51858d549cd340759
ryangabriel142 <48722498+ryangabriel142@users.noreply.github.com>
2019-04-09 11:51:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+	<title>
+		Projeto App Minha Casa minha Vida
+	</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
+	</head>
+<body>
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+	<ul>
+<li><a href="index.php">Home<a/></li>	
+<li><a href="perfil.php">Perfil<a/></li>	
+<li><a href="contato.php">Contato<a/></li>	
+	</ul>	
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Rafael A. Florindo<br>
+		<a href="mailto:rafael.florindo@escola.pr.gov.br">Contato</a>
+	</div>
+</body>
+</html>
